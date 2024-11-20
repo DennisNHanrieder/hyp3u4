3eb09b8ed9ebf3db25ba5fa561efdad6d37da896
Dennsi <mr.dusky@outlook.de>
2024-11-20 13:07:19 +0100

    kinda done not really tho

--- script.js
+++ script.js
@@ -1,47 +1,63 @@
 "use strict";
-/*
- * Hypermedia Systems & Architecture
- * http://www.fh-ooe.at/mtd
- *
- * Simple Vue.js 3 Application Template
- *
- */
 
-const STORAGE_KEY = "hyp3t1.lab2.g1.2024";
+class Person {
+    constructor(firstname, lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+}
 
-Vue.createApp({
+const app = Vue.createApp({
     data() {
         return {
-		    notesData: [],
-            contents: "",
-        }
-	},
+            people: [],
+            newFirstname: "",
+            newLastname: "",
+        };
+    },
     methods: {
-        saveData() {
-            if (this.contents.trim().length === 0) {
-                this.contents = "";
-                return;
+        addPerson() {
+            if (this.newFirstname.trim() && this.newLastname.trim()) {
+                const newPerson = new Person(this.newFirstname.trim(), this.newLastname.trim());
+                this.people.push(newPerson);
+                this.newFirstname = "";
+                this.newLastname = "";
             }
-
-            const entry = {
-                date: new Date().toLocaleString(),
-                contents: this.contents.trim(),
-            };
-
-            this.notesData.push(entry);
-            this.contents = "";
-
-            localStorage.setItem(STORAGE_KEY,
-                JSON.stringify(this.notesData)
-            );
         },
-    },
-    created() {
-        const data = localStorage.getItem(STORAGE_KEY);
+        removePerson(index) {
+            this.people.splice(index, 1);
+        },
+        updatePerson(index, updatedPerson) {
+            this.people[index] = updatedPerson;
+        },
+        clearPeople() {
+            this.people = [];
+        }
+    }
+});
 
-        if (data) {
-            this.notesData = JSON.parse(data);
-            if (!Array.isArray(this.notesData)) this.notesData = [];
+app.component("person-item", {
+    props: ["firstname", "lastname"],
+    emits: ["delete-person", "update-person"],
+    data() {
+        return {
+            isEditing: false,
+            editedFirstname: this.firstname,
+            editedLastname: this.lastname
+        };
+    },
+    methods: {
+        startEditing() {
+            this.isEditing = true;
+        },
+        saveEdit() {
+            if (this.editedFirstname.trim() && this.editedLastname.trim()) {
+                this.$emit("update-person", new Person(this.editedFirstname.trim(), this.editedLastname.trim()));
+            }
+            this.isEditing = false;
         }
     },
-}).mount('#app');
+    template: "#person-template"
+});
+
+app.mount("#app");
